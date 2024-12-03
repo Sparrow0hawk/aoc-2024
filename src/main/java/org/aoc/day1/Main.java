bee3e159ee30fce4dd0314bf60aa41c51a75369c
Alex Coleman <hello@alexjcoleman.me>
2024-12-03 20:51:00 +0000

    Camel case variable name

--- src/main/java/org/aoc/day1/Main.java
+++ src/main/java/org/aoc/day1/Main.java
@@ -8,13 +8,13 @@ public class Main {
     public static void solve() throws FileNotFoundException {
         BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/day1.txt"));
 
-        HistoricalLocations hist_locations = new HistoricalLocations();
+        HistoricalLocations historicalLocations = new HistoricalLocations();
 
-        reader.lines().forEach(hist_locations::add_line);
+        reader.lines().forEach(historicalLocations::add_line);
 
-        int result = hist_locations.distances().stream().mapToInt(Integer::intValue).sum();
+        int result = historicalLocations.distances().stream().mapToInt(Integer::intValue).sum();
 
-        int partTwoResult = hist_locations.similarities().stream().mapToInt(Math::toIntExact).sum();
+        int partTwoResult = historicalLocations.similarities().stream().mapToInt(Math::toIntExact).sum();
 
         System.out.println("Solution for day 1 part 1: " + result);
         System.out.println("Solution for day 1 part 2: " + partTwoResult);
