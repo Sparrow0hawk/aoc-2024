db2ee902c0dfe596bbd04d09f2090fbb89ac11de
Alex Coleman <hello@alexjcoleman.me>
2024-12-08 20:55:44 +0000

    Store original levels

--- src/main/java/org/aoc/day2/SafetyReport.java
+++ src/main/java/org/aoc/day2/SafetyReport.java
@@ -5,22 +5,25 @@ import java.util.*;
 import static java.lang.Math.abs;
 
 public class SafetyReport {
-    private List<Integer> levelChanges;
+    private List<Integer> levels;
 
     public SafetyReport(String line) {
-        List<Integer> levels = Arrays.stream(line.split("\\s+")).map(Integer::parseInt).toList();
+        levels = Arrays.stream(line.split("\\s+")).map(Integer::parseInt).toList();
+    }
 
+    private List<Integer> calculateChanges(List<Integer> levels) {
         List<Integer> changes = new ArrayList<>();
         for (int i = 0; i < levels.size(); i++) {
             if (i + 1 < levels.size()) {
                 changes.add(levels.get(i) - levels.get(i + 1));
             }
         }
-        levelChanges = changes;
+        return changes;
     }
 
     public boolean isSafe() {
         HashSet<Integer> acceptable_change = new HashSet<>(Arrays.asList(1, 2, 3));
+        List<Integer> levelChanges = this.calculateChanges(levels);
         return (levelChanges.stream().allMatch(i -> i < 0) | levelChanges.stream().allMatch(i -> i > 0)) & levelChanges.stream().allMatch(i -> acceptable_change.contains(abs(i)));
     }
 }
